9415743301e3b5aaf59fbf86786a231dcc975464
diegobatiz <diegow.batiz@gmail.com>
2024-10-14 14:37:21 -0700

    bug fix and project done

--- Framework/Core/Src/BlockAllocator.cpp
+++ Framework/Core/Src/BlockAllocator.cpp
@@ -19,7 +19,7 @@ BlockAllocator::BlockAllocator(const char* name, size_t blockSize, size_t capaci
 
 	for (size_t i = 0; i < capacity; ++i)
 	{
-		mFreeBlocks[i] = static_cast<uint32_t*>(mData) + (i + blockSize);
+		mFreeBlocks[i] = static_cast<uint8_t*>(mData) + (i * blockSize);
 	}
 
 	LOG("%s allocated %zu block, blocks size: %zu", mName.c_str(), capacity, blockSize);
@@ -45,9 +45,10 @@ void* BlockAllocator::Allocate()
 
 	void* freeBlock = mFreeBlocks.back();
 	mFreeBlocks.pop_back();
+
 	++mBlocksAllocatedTotal;
 	++mBlocksAllocatedCurrent;
-	++mBlocksHighest = std::max(mBlocksHighest, mBlocksAllocatedCurrent);
+	mBlocksHighest = std::max(mBlocksHighest, mBlocksAllocatedCurrent);
 
 	LOG("%s allocated blocks at %p, Allocated: %zu, Highest: %zu",
 		mName.c_str(), freeBlock, mBlocksAllocatedCurrent, mBlocksHighest);
@@ -66,7 +67,7 @@ void BlockAllocator::Free(void* ptr)
 	const uint8_t* end = static_cast<uint8_t*>(mData) + (mBlockSize * mCapacity);
 	const uint8_t* current = static_cast<uint8_t*>(ptr);
 	const auto diff = current - start;
-	ASSERT(current >= start && current < end&& static_cast<size_t>(diff) % mBlockSize == 0,
+	ASSERT(current >= start && current < end && static_cast<size_t>(diff) % mBlockSize == 0,
 		"BlockAllocator: %s invalid address being freed!", mName.c_str());
 
 	LOG("%s free %p", mName.c_str(), ptr);
--- VGP336/23_HelloMemory/WinMain.cpp
+++ VGP336/23_HelloMemory/WinMain.cpp
@@ -24,40 +24,120 @@ private:
 	std::string mName;
 };
 
+class Lion
+{
+public:
+	Lion(const std::string& name, int id, const std::string& food)
+		: mName(name)
+		, mId(id)
+		, mFavouriteFood(food)
+	{
+	}
+
+private:
+	int mId = 0;
+	std::string mName;
+	std::string mFavouriteFood;
+};
+
+class Song
+{
+public:
+	Song(const std::string& title, const std::string& artist, float length)
+		: mTitle(title)
+		, mArtistName(artist)
+		, mLength(length)
+	{
+	}
+
+private:
+	float mLength;
+	std::string mTitle;
+	std::string mArtistName;
+};
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR lpCmdLine, _In_ int nCmdShow)
 {
-	TypedAllocator studentPool = TypedAllocator<Student>("StudentPool", 250);
+	TypedAllocator studentPool = TypedAllocator<Student>("StudentPool", 200);
+	TypedAllocator lionPool = TypedAllocator<Lion>("Animal Pool", 150);
+	TypedAllocator songPool = TypedAllocator<Song>("Song Pool", 400);
+
+	int size = sizeof(Student);
 
 	std::vector<Student*> students;
+	std::vector<Lion*> lions;
+	std::vector<Song*> songs;
 
 	//test new
-	for (uint32_t i = 0; i < 100; ++i)
+	for (uint32_t i = 0; i < 10; ++i)
 	{
 		Student* newStudent = studentPool.New("Student_" + std::to_string(i), i);
+		Lion* lion = lionPool.New("Lion_" + std::to_string(i), i, "Meat");
 		students.push_back(newStudent);
+		lions.push_back(lion);
+	}
+	for (uint32_t i = 0; i < 350; ++i)
+	{
+		Song* newSong = songPool.New("Song No. " + std::to_string(i), "Diego Batiz", 90.0f + i * 0.1f);
+		songs.push_back(newSong);
 	}
 
 	//test deletion
-	for (uint32_t i = 0; i < 20; ++i)
+	for (uint32_t i = 0; i < 5; ++i)
 	{
-		auto student = students.back();
+		auto& student = students.back();
 		studentPool.Delete(student);
 		students.pop_back();
 	}
+	for (uint32_t i = 0; i < 5; ++i)
+	{
+		auto& lion = lions.back();
+		lionPool.Delete(lion);
+		lions.pop_back();
+	}
+	for (uint32_t i = 0; i < 100; ++i)
+	{
+		auto& song = songs.back();
+		songPool.Delete(song);
+		songs.pop_back();
+	}
 
-	//add new students
+	//add new items
 	for (uint32_t i = 0; i < 150; ++i)
 	{
 		Student* newStudent = studentPool.New("Student_" + std::to_string(i));
 		students.push_back(newStudent);
 	}
+	for (uint32_t i = 0; i < 50; ++i)
+	{
+		Lion* lion = lionPool.New("Lion_" + std::to_string(i), i, "Meat");
+		lions.push_back(lion);
+	}
+	for (uint32_t i = 0; i < 150; ++i)
+	{
+		Song* newSong = songPool.New("Song No. " + std::to_string(i), "Diego Batiz", 90.0f + i * 0.1f);
+		songs.push_back(newSong);
+	}
+
 
 	for (auto& student : students)
 	{
 		studentPool.Delete(student);
 	}
 
+	for (auto& lion : lions)
+	{
+		lionPool.Delete(lion);
+	}
+
+	for (auto& song : songs)
+	{
+		songPool.Delete(song);
+	}
+
 	students.clear();
+	lions.clear();
+	songs.clear();
 
 	return 0;
 }
