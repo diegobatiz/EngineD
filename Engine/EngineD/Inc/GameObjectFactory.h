e254c16673128f2e8616ec316bc46b4260e462a7
diegobatiz <diegow.batiz@gmail.com>
2024-11-06 21:43:31 -0800

    wave loader

--- Engine/EngineD/Inc/GameObjectFactory.h
+++ Engine/EngineD/Inc/GameObjectFactory.h
@@ -4,14 +4,14 @@ namespace EngineD
 {
 	class GameObject;
 
-	using CustomMake = std::function <Component*(const std::string&, GameObject&)>;
-	using CustomGet = std::function <Component*(const std::string&, GameObject&)>;
+	//using CustomMake = std::function <Component*(const std::string&, GameObject&)>;
+	//using CustomGet = std::function <Component*(const std::string&, GameObject&)>;
 
 	namespace GameObjectFactory
 	{
 		void Make(const std::filesystem::path& templatePath, GameObject& gameObject);
 		void OverrideDeserialize(const rapidjson::Value& value, GameObject& gameObject);
-		void SetCustomMake(CustomMake);
-		void SetCustomGet(CustomGet);
+		//void SetCustomMake(CustomMake);
+		//void SetCustomGet(CustomGet);
 	}
 }
